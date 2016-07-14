80574469fe8ee4eb99d6e396819a2da85ddca600
Shadman <shadman.jamil@tenpearls.com>
2016-07-14 15:20:01 +0500

    validation added

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -2,6 +2,24 @@ $(document).ready(function() {
 
      new Clipboard('.btn');
 
+     $('.json-form').submit(function(){
+
+          if ($('#resume-json').val() == '') {
+            $('#message').html("Please paste/write your resume JSON to generate.");
+            return false;
+          } 
+          else {
+            try {
+                JSON.parse($('#resume-json').val());
+                return true;
+            } catch (e) {
+                $('#message').html("Please add a valid JSON to generate your resume.");
+                return false;
+            }  
+          }
+
+
+     });
 
 });
    
