cf84721e334a839313ed0700bf15725cf8f39284
Shadman <shadman.jamil@tenpearls.com>
2016-07-14 11:49:04 +0500

    renamed main page; added ruby json parser to render json

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -3,14 +3,18 @@ class ResumeController < ApplicationController
   def index
   	@cool = 'Shadman'
   	
-  	render :index
+  	render :form
   end
 
   def parse
   	@cool = 'Jamil'
 
-  	puts params[:resumejson]
-  	
+  	resume_json = params[:resumejson]
+  	resume_data = JSON.parse(resume_json)
+	#@resume = resume_data['basic_information']['first_name']
+	appModel = ApplicationRecord.new
+	@resume = resume_data #appModel.parse_json(resume_data)
+
   	render :preview
   end
 
